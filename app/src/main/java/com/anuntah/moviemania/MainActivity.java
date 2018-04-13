63874aba75779b7109927c3b515065915d8d0059
Bhavit Yadav <bhavit.yadav@gmail.com>
2018-04-13 14:24:35 +0530

    Movie trailer layout

--- app/src/main/java/com/anuntah/moviemania/MainActivity.java
+++ app/src/main/java/com/anuntah/moviemania/MainActivity.java
@@ -1,14 +1,17 @@
 package com.anuntah.moviemania;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private BottomNavigationView bottomNavigationView;
+    private BottomNavigationView bottomNavigationView;
     private FrameLayout frameLayout;
 
     private ViewPager mSlideViewPager;
@@ -20,42 +23,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_movies);
+        setContentView(R.layout.activity_main);
 
-        mSlideViewPager = findViewById(R.id.slideViewPages);
-        mDotsLayout = findViewById(R.id.dotsLayout);
+        bottomNavigationView=findViewById(R.id.mainNav);
 
-        /*bottomNavigationView = findViewById(R.id.mainNav);
-        frameLayout = findViewById(R.id.mainFrame);*/
-
-        mSlideViewPager = findViewById(R.id.slideViewPages);
-        mDotsLayout = findViewById(R.id.dotsLayout);
-
-
-        /*bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
 
                     case R.id.movie_btn:
-                        frameLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                         return true;
 
                     case  R.id.tv_btn:
-                        frameLayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
                         return true;
                     case R.id.profile_btn:
-                        frameLayout.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                         return true;
                 }
                 return true;
             }
-        });*/
-
-
-        /*slideAdapter = new SlideAdapter(this);
-        mSlideViewPager.setAdapter(slideAdapter);*/
-
+        });
 
     }
 }
